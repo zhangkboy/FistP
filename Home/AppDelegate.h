e554f1f9fa1efbf47fdc87ee535f8b4951cc07cb
zhangkboy <zhangkboy@icloud.com>
2016-01-10 18:09:30 +0800

    +

---
+++ Home/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MTPSocial
+//
+//  Created by ZhangKaiChao on 16/1/10.
+//  Copyright © 2016年 北京欧创医疗技术有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Home/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  Home
+//
+//  Created by ZhangKaiChao on 16/1/10.
+//  Copyright © 2016年 北京欧创医疗技术有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
